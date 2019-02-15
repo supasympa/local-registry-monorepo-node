6f5d52d46ceffae5c576ce6ff5f9848cb32f99de
Lewis Barclay <lewis.barclay@googlemail.com>
2019-02-15 21:11:47 +0000

    Added a domain object to test

--- packages/domain/spec/helloWorld.spec.ts
+++ packages/domain/spec/helloWorld.spec.ts
@@ -1,9 +1,10 @@
 import * as chai from 'chai';
+import { helloWorld } from "../src/helloWorld";
 
 const { expect } = chai;
 
 describe('Hello World', () => {
     it('should be testable', () => {
-        expect(true).to.equal(true);
+        expect(helloWorld()).to.equal('Hello World!');
     })
 });
---
+++ packages/domain/src/helloWorld.ts
@@ -0,0 +1,2 @@
+
+export const helloWorld = () => `Hello World!`;
